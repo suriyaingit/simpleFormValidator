457a17f327d56624952be046748e75537fa0cd51
JsDevHub <jsdevhub@gmail.com>
2016-10-19 17:22:29 +0530

    Update sf-validator-0.1.js

--- sf-validator-0.1.js
+++ sf-validator-0.1.js
@@ -1,1 +1,152 @@
-var SimpleFormValidator=function(){function g(a){var b="",c="",d="",e=[];return $("form#"+a+" .form-fields").each(function(){b=$(this).attr("req"),c=$(this).attr("msg"),d=$(this).attr("len"),e.push({fieldName:$(this).attr("name"),required:b,message:c,length:d})}),e}function i(a,b,c){b?(a.css({"border-color":e.errorHandler.errorBgColor}),a.next().show().find(".error").css({color:e.errorHandler.errorMsgColor}).html(c).show()):(a.css({"border-color":""}),a.next().hide())}function j(a){return"undefined"==typeof a}var a='<div class="validation"><span class="error"></span></div>',b=[],c=[],d=0,e={errorHandler:{errorBgColor:"red",errorMsgColor:"red"}},f=function(a){j(a)||($.extend(e,a),$("#"+e.formId).on("submit",function(a){j(e.rules)&&(e.rules=g(e.formId)),0==h(a,e)&&(j(e.sucsuccessHandler)||e.successHandler($("#"+e.formId)))}))},h=function(e,f){return d=0,j(e)||e.preventDefault(),$("form#"+f.formId+" .form-fields").each(function(){b.push($(this)),c.push($(this).prop("name")),$(this).after(a)}),$.each(f.rules,function(a,e){var f=$.inArray(e.fieldName,c);if(f>=0){var g=b[f];""==g.val().trim()&&e.required||"radio"==g.prop("type")&&!g.prop("checked")&&e.required||"checkbox"==g.prop("type")&&!g.prop("checked")&&e.required?(i(g,!0,e.message),d++):i(g,!1,e.message),e.trimed&&"text"==g.type&&g.val($.trim(g.val())),g.val().trim().length>e.length&&"text"==g.type&&(i(g,!0,"Field Length Exceeds to "+g.val().length+" expected is "+e.length+"."),d++)}}),d};return{validate:f}}();
+var SimpleFormValidator = (function() {
+
+	var errorDiv = '<div class="validation"><span class="error"></span></div>';
+
+	var elementsArray = [];
+	var eleNameArray = [];
+	var errorCount = 0;
+	var config = {
+		errorHandler : {
+			errorBgColor : 'red',
+			errorMsgColor : 'red'
+		}
+	};
+
+	var validate = function(cfg) {
+
+		if (!isNull(cfg)) {
+
+			$.extend(config, cfg);
+
+			$('#' + config.formId).on('submit', function(event) {
+
+				if (isNull(config.rules)) {
+					config.rules = getFormConfigDetails(config.formId);
+				}
+
+				if (validateFormData(event, config) == 0) {
+
+					if (!isNull(config.sucsuccessHandler))
+						config.successHandler($('#' + config.formId));
+
+				}
+			});
+
+		}
+
+	}
+
+	function getFormConfigDetails(formId) {
+
+		var req = '';
+		var msg = '';
+		var len = '';
+
+		var configDetails = [];
+
+		$("form#" + formId + " .form-fields").each(function() {
+
+			req = $(this).attr('req');
+			msg = $(this).attr('msg');
+			len = $(this).attr('len');
+
+			configDetails.push({
+				fieldName : $(this).attr('name'),
+				required : req,
+				message : msg,
+				length : len
+			});
+
+		});
+
+		return configDetails;
+	}
+
+	// ----------- Validation Functions ------------ //
+
+	var validateFormData = function(event, config) {
+
+		errorCount = 0;
+
+		if (!isNull(event))
+			event.preventDefault();
+
+		// get all form inputs in array
+		$("form#" + config.formId + " .form-fields").each(function() {
+			
+			elementsArray.push($(this));
+			eleNameArray.push($(this).prop('name'));
+			$(this).after(errorDiv);
+		});
+
+		$.each(config.rules,
+						function(i, rule) {
+
+							var index = $.inArray(rule.fieldName, eleNameArray);
+
+							if (index >= 0) {
+								var ele = elementsArray[index];
+
+								// validation logic 1 - null check
+								if ((ele.val().trim() == "" && rule.required)
+										|| (ele.prop('type') == 'radio'
+												&& !ele.prop('checked') && rule.required)
+										|| (ele.prop('type') == 'checkbox'
+												&& !ele.prop('checked') && rule.required)) {
+									showErrors(ele, true, rule.message);
+									errorCount++;
+								} else {
+									showErrors(ele, false, rule.message);
+								}
+
+								// validation logic 2 - trim out value if
+								// contain any spaces
+								if (rule.trimed && ele.type == 'text')
+									ele.val($.trim(ele.val()));
+
+								// validation logic 3 - input length check
+								if (ele.val().trim().length > rule.length
+										&& ele.type == 'text') {
+									showErrors(ele, true,
+											'Field Length Exceeds to '
+													+ ele.val().length
+													+ ' expected is '
+													+ rule.length + '.');
+									errorCount++;
+								}
+							}
+						});
+
+		return errorCount;
+	}
+
+	function showErrors(ele, errorFlag, message) {
+		if (errorFlag) {
+			ele.css({
+				"border-color" : config.errorHandler.errorBgColor
+			});
+			ele.next().show().find('.error').css({
+				"color" : config.errorHandler.errorMsgColor
+			}).html(message).show();
+
+		} else {
+			ele.css({
+				"border-color" : ""
+			});
+			ele.next().hide();
+		}
+	}
+
+	function isNull(property) {
+		if (typeof (property) !== 'undefined') {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	return {
+		'validate' : validate
+	}
+
+}());
